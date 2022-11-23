676a4bbe026f6dc42dd7ed14ff4eac639ab89e19
Mahdi Bozorgi <102772767+Mehtisup@users.noreply.github.com>
2022-11-23 20:54:40 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+document.getElementById("btn").onclick = function() {myFunction()};
+
+function myFunction() {
+    document.getElementById("header").innerHTML = "Hello World";
+}
+</script>
